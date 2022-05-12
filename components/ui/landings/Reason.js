47411d1b87db60feb41489ff7bc0dfd4a76deb68
Tarik Bin Shams <binshams.aiub@gmail.com>
2022-05-12 17:55:44 +0600

    First commit

---
+++ components/ui/landings/Reason.js
@@ -0,0 +1,64 @@
+export default function Reason() {
+    return (
+        <div className="grid gap-4 sm:grid-cols-2 my-16">
+            <div className="col-span-1 flex sm:justify-end justify-center">
+                <div className="grid gap-1 sm:grid-cols-3 sm:ml-16">
+                    <div className="col-span-1 sm:mt-24">
+                        <div className="border border-gray-400 rounded-md py-6 px-2 mx-2 hover:border-blue-400">
+                            <p className="absolute -mt-6 text-orange-500 text-sm font-semibold">01</p>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="mx-auto h-8 w-8 text-blue-700" viewBox="0 0 20 20" fill="currentColor">
+                                <path fillRule="evenodd" d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z" clipRule="evenodd" />
+                                <path d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z" />
+                            </svg>
+                            <p className="text-md text-center font-bold">Reliability</p>
+                            <p className="text-xs text-gray-500 text-center mt-4">It is a long established fact that a reader will be distracted.</p>
+                        </div>
+                    </div>
+                    <div className="col-span-1">
+                        <div className="grid grid-row-2 gap-3">
+                            <div className="border border-gray-400 rounded-md py-6 px-2 mx-2 hover:border-blue-400">
+                                <p className="absolute -mt-6 text-orange-500 text-sm font-semibold">02</p>
+                                <svg xmlns="http://www.w3.org/2000/svg" className="mx-auto h-8 w-8 text-orange-700" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fillRule="evenodd" d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z" clipRule="evenodd" />
+                                    <path d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z" />
+                                </svg>
+                                <p className="text-md text-center font-bold">Resonable fess</p>
+                                <p className="text-xs text-gray-500 text-center mt-4">It is a long established fact that a reader will be distracted.</p>
+                            </div>
+                            <div className="border border-gray-400 rounded-md py-6 px-2 mx-2 hover:border-blue-400">
+                                <p className="absolute -mt-6 text-orange-500 text-sm font-semibold">03</p>
+                                <svg xmlns="http://www.w3.org/2000/svg" className="mx-auto h-8 w-8 text-orange-700" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fillRule="evenodd" d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z" clipRule="evenodd" />
+                                    <path d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z" />
+                                </svg>
+                                <p className="text-md text-center font-bold">Resonable fess</p>
+                                <p className="text-xs text-gray-500 text-center mt-4">It is a long established fact that a reader will be distracted.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-span-1 sm:mt-24">
+                        <div className="border border-gray-400 rounded-md py-6 px-2 mx-2 hover:border-blue-400">
+                            <p className="absolute -mt-6 text-orange-500 text-sm font-semibold">04</p>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="mx-auto h-8 w-8 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
+                                <path fillRule="evenodd" d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z" clipRule="evenodd" />
+                                <path d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z" />
+                            </svg>
+                            <p className="text-md text-center font-bold">Availability</p>
+                            <p className="text-xs text-gray-500 text-center mt-4">It is a long established fact that a reader will be distracted.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="col-span-1 h-auto -z-10 flex justify-end">
+                <div className="sm:absolute bg-[url('../public/assets/landings/reasonVector.png')] bg-no-repeat bg-cover sm:w-6/12 sm:h-[44rem] h-screen sm:-mt-72">
+                    <div className="mt-64 sm:mt-80 sm:ml-28 ml-16 mr-8">
+                        <p className="text-white sm:mt-48 text-[2rem] font-semibold">Why customer</p>
+                        <p className="text-white text-[2rem] font-semibold">choose ESCROW?</p>
+                        <p className="text-white text-sm mt-4 font-normal">Yet bed any for travelling assistance indulgence unpleasing.<br /> Not thoughts all excercise blessing.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,45 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Landings.module.css';
+import Reason from '../components/ui/landings/Reason.js';
+export default function Home() {
+  return (
+    <div className='' >
+      <div className="bg-[url('../public/assets/landings/topBackground.png')] h-[40rem] bg-center bg-no-repeat bg-cover">
+        <div className='flex justify-around pt-10'>
+          <div className='text-3xl text-white font-bold'>ESCROW</div>
+          <div className="mt-2">
+            <ul className='hidden md:flex text-sm text-white'>
+              <li className='mx-6 cursor-pointer'>Consumer</li>
+              <li className='mx-6 cursor-pointer'>Boardless Account</li>
+              <li className='mx-6 cursor-pointer'>Business</li>
+              <li className='mx-6 cursor-pointer'>Help</li>
+              <button className='mx-6 border border-white px-6 bg-white text-black text-xs py-1 rounded-full hover:bg-[#27426f] hover:text-white transition ease-in-out'>Login</button>
+            </ul>
+          </div>
+        </div>
+        <div className='mt-36 md:mt-40'>
+          <p className='w-64 md:w-80 mx-auto text-3xl font-semibold text-white text-center align-middle'>A secure, reliable way to transact your money</p>
+          <p className='w-80 md:w-[36rem] mx-auto text-md font-light mt-4 text-white text-center align-middle'>It is a long established fact that a reader will be distracted by the readable
+            content of a page when looking at its layout. The point of using Lorem Ipsum is </p>
+        </div>
+      </div>
+      <div className='flex justify-center items-center -mt-44 z-50'>
+        <div className="w-4/5 md:w-2/3 p-3">
+          <div className="flex flex-col lg:flex-row rounded-2xl overflow-hidden h-auto shadow shadow-lg">
+            <img className="block h-64 md:h-auto w-full md:w-2/4 flex-none bg-cover" src="/assets/landings/humanImage.png" />
+            <div className="bg-white -ml-4 rounded-b lg:rounded-b-none lg:rounded-r px-8 py-8 flex flex-col justify-between leading-normal">
+              <p className="text-xl text-gray-500">What is a</p>
+              <p className="text-3xl font-bold text-blue-900 md:-mt-4">Secure transaction?</p>
+              <p className="text-xs text-gray-500">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is </p>
+              <p className="text-xs text-gray-500 mt-2">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is </p>
+              <button className='w-2/4 md:w-1/4 py-4 mt-4 rounded-lg text-center border border-orange-500 text-orange-500 font-semibold text-xs hover:text-white hover:bg-orange-500 transition ease-in-out'>Learn More..</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Reason />
+      <div className='h-96 bg-blue-900'></div>
+    </div>
+  )
+}
